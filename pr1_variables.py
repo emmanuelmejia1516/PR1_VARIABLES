87bf8115c467358df6694dbb665dca1067d63862
emmanuelmejia1516 <a.23308051281197@cbtis128.edu.mx>
2024-09-23 20:27:59 -0600

    Add files via upload

---
+++ pr1_variables.py
@@ -0,0 +1,11 @@
+#ejemplo de variables
+#Asignación de valores a las variables
+x = 10  # x almacena el valor 10
+y = 20  # y almacena el valor 20
+
+#Impresión de las variables
+print(x)  # Imprime el valor de x
+print(y)  # Imprime el valor de y
+print("")
+print("mejia suarez emmanuel alexander:mi practica es de asignacion de variables")
+print("")
